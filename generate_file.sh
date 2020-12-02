76eb507a0798a636d6fa861f30a5d57854413ce9
whn <me@whn.se>
2020-12-02 23:57:00 +0100

    add generator script

---
+++ generate_file.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+export DAY=$(date "+%-d")
+
+EXERCISE=$(curl -s -b $(cat cookies.txt) "https://adventofcode.com/2020/day/$DAY" | sed -n '/^<main>/,/^<\/main>/p;/^<\/main>/q' | html2text -nobs -width 75)
+
+export EXERCISE_COMMENT=$EXERCISE
+
+
+DATA=$()
+
+FILE="src/day${DAY}.rs"
+if test -f $FILE; then
+    echo "$FILE exists already."
+    BACKUP="src/day${DAY}_from_template.rs.bak"
+    cat ./template.rs.tpls  | envsubst '${EXERCISE_COMMENT},${DAY}' > $BACKUP
+    echo "Wrote to $BACKUP."
+    exit 0
+fi
+
+cat ./template.rs.tpls  | envsubst '${EXERCISE_COMMENT},${DAY}' > $FILE
+
+echo "Wrote to $FILE."
